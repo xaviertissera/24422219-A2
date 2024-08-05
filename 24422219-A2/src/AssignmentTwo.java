5bd27e4f00e08b070ac6cfc8d691ae247c2d1ef5
xaviertissera <78007807+xaviertissera@users.noreply.github.com>
2024-08-05 19:29:59 +1100

    employee class part 1

--- 24422219-A2/src/AssignmentTwo.java
+++ 24422219-A2/src/AssignmentTwo.java
@@ -1,5 +1,8 @@
 public class AssignmentTwo {
     public static void main(String[] args) {
 
+        Person person = new Person("Xavier Tissera", 25, "Male");
+
+
     }
 }
--- 24422219-A2/src/Employee.java
+++ 24422219-A2/src/Employee.java
@@ -1,2 +1,35 @@
-public class Employee {
+public class Employee extends Person {
+    private String employeeId;
+    private String position;
+
+    // Default
+    public Employee() {
+    }
+
+    // With parameters
+    public Employee(String name, int age, String gender, String employeeId, String position) {
+        super(name, age, gender);
+        this.employeeId = employeeId;
+        this.position = position;
+    }
+
+    // Getters and Setters
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+
+
 }
