ae7f3bc7c8e6d61dd52eb9b7920e4629bf311205
xaviertissera <78007807+xaviertissera@users.noreply.github.com>
2024-08-05 20:58:04 +1100

    end of part 1

--- 24422219-A2/src/Ride.java
+++ 24422219-A2/src/Ride.java
@@ -1,2 +1,46 @@
 public class Ride {
+    private String rideName;
+    private int capacity;
+    private Employee operator;  // This variable will be used to know if the ride is open and who is operating it
+
+    // Default constructor
+    public Ride() {
+    }
+
+    // Constructor parameters
+    public Ride(String rideName, int capacity, Employee operator) {
+        this.rideName = rideName;
+        this.capacity = capacity;
+        this.operator = operator;
+    }
+
+    // Getters and Setters
+    public String getRideName() {
+        return rideName;
+    }
+
+    public void setRideName(String rideName) {
+        this.rideName = rideName;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Employee getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Employee operator) {
+        this.operator = operator;
+    }
+
+    // Method to assign an Employee to operate the ride
+    public void assignOperator(Employee operator) {
+        this.operator = operator;
+    }
 }
--- 24422219-A2/src/Visitor.java
+++ 24422219-A2/src/Visitor.java
@@ -1,2 +1,32 @@
-public class Visitor {
+public class Visitor extends Person {
+    private String visitorId;
+    private String ticketType;
+
+    // Default constructor
+    public Visitor() {
+    }
+
+    // Constructor with parameters
+    public Visitor(String name, int age, String gender, String visitorId, String ticketType) {
+        super(name, age, gender);
+        this.visitorId = visitorId;
+        this.ticketType = ticketType;
+    }
+
+    // Getters and Setters
+    public String getVisitorId() {
+        return visitorId;
+    }
+
+    public void setVisitorId(String visitorId) {
+        this.visitorId = visitorId;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public void setTicketType(String ticketType) {
+        this.ticketType = ticketType;
+    }
 }
