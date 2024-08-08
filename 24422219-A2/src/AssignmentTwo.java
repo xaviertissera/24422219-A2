13a5136541027f8d074d0399f9741a86b17891a2
xaviertissera <78007807+xaviertissera@users.noreply.github.com>
2024-08-09 02:44:34 +1100

    Checking the initial stage

--- 24422219-A2/src/AssignmentTwo.java
+++ 24422219-A2/src/AssignmentTwo.java
@@ -3,6 +3,23 @@ public class AssignmentTwo {
 
         Person person = new Person("Xavier Tissera", 25, "Male");
 
+        // Employee instance
+        Employee employee = new Employee("Jack Greg", 28, "Female", "E123", "Ride Operator");
+
+        // Visitor instance
+        Visitor visitor = new Visitor("Alice Johnson", 25, "Female", "V456", "VIP");
+
+        // Ride instance
+        Ride ride = new Ride("Boomerang Coaster", 20, employee);
+
+        // Display output
+        System.out.println("Person Name: " + person.getName());
+        System.out.println("Employee Name: " + employee.getName() + ", Position: " + employee.getPosition());
+        System.out.println("Visitor Name: " + visitor.getName() + ", Ticket Type: " + visitor.getTicketType());
+        System.out.println("Ride Name: " + ride.getRideName() + ", Operator: " + ride.getOperator().getName());
+
+
+
 
     }
 }
