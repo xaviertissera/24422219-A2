aa844a7d0daedd715d33d4415273a1a397701cd7
xaviertissera <78007807+xaviertissera@users.noreply.github.com>
2024-08-09 17:12:47 +1100

    part 5
    implementing max visitors who can take the ride in a 1 cycle

--- 24422219-A2/src/AssignmentTwo.java
+++ 24422219-A2/src/AssignmentTwo.java
@@ -5,12 +5,14 @@ public class AssignmentTwo {
         assignment.partThree();
         assignment.partFourA();
         assignment.partFourB();
+        assignment.partFive();
     }
 
     public void partThree() {
         // Part 3
         // New Ride object with Variables
-        Ride boomerangCoaster = new Ride("Boomerang Coaster", 2, new Employee("Jane Smith", 28, "Female", "E123", "Ride Operator"));
+        // Updated for part 5 including new variables
+        Ride boomerangCoaster = new Ride("Boomerang Coaster", 2, new Employee("Jane Smith", 28, "Female", "E123", "Ride Operator"), 2);
 
         // Create five Visitor objects
         Visitor visitor1 = new Visitor("Jack", 30, "Male", "V001", "Regular");
@@ -25,12 +27,17 @@ public class AssignmentTwo {
         boomerangCoaster.AddVisitorToQueue(visitor3);
         boomerangCoaster.AddVisitorToQueue(visitor4);
         boomerangCoaster.AddVisitorToQueue(visitor5);
+        System.out.println();
 
         // Remove a Visitor from the Queue
         boomerangCoaster.RemoveVisitorFromQueue(visitor3);
+        System.out.println();
 
         // Print all Visitors currently available in the Queue
         boomerangCoaster.PrintQueue();
+        System.out.println();
+
+        System.out.println("PART 4A---------------------------------");
 
 
     }
@@ -38,7 +45,7 @@ public class AssignmentTwo {
     public void partFourA() {
         // Part 4A
         // New Ride object
-        Ride supermanEscape = new Ride("Superman Escape", 4, new Employee("John Doe", 35, "Male", "E124", "Ride Operator"));
+        Ride supermanEscape = new Ride("Superman Escape", 4, new Employee("John Doe", 35, "Male", "E124", "Ride Operator"), 4);
 
         // Visitor objects
         Visitor visitor1 = new Visitor("Lee", 40, "Male", "V006", "Regular");
@@ -53,22 +60,28 @@ public class AssignmentTwo {
         supermanEscape.addVisitorToRideHistory(visitor3);
         supermanEscape.addVisitorToRideHistory(visitor4);
         supermanEscape.addVisitorToRideHistory(visitor5);
+        System.out.println();
 
         // Checking for a Visitor is in the Ride history
         supermanEscape.isVisitorInRideHistory(visitor3);
+        System.out.println();
 
         // Number of Visitors in the history
         supermanEscape.getNumberOfVisitorsInRideHistory();
+        System.out.println();
 
         // Print all the Visitors in the Ride history
         supermanEscape.PrintRideHistory();
+        System.out.println();
+
+        System.out.println("PART 4B---------------------------------");
 
     }
 
     public void partFourB() {
         // Part 4B
         // New Ride object
-        Ride kansasTwister = new Ride("Kansas Twister", 3, new Employee("Alice Smith", 30, "Female", "E125", "Ride Operator"));
+        Ride kansasTwister = new Ride("Kansas Twister", 3, new Employee("Alice Smith", 30, "Female", "E125", "Ride Operator"), 3);
 
         // New Visitor objects
         Visitor visitor1 = new Visitor("Zara", 22, "Female", "V011", "Regular");
@@ -83,17 +96,75 @@ public class AssignmentTwo {
         kansasTwister.addVisitorToRideHistory(visitor3);
         kansasTwister.addVisitorToRideHistory(visitor4);
         kansasTwister.addVisitorToRideHistory(visitor5);
+        System.out.println();
 
         // Printing all Visitors in the Ride history
         System.out.println("Before sorting:");
         kansasTwister.PrintRideHistory();
+        System.out.println();
 
         // Sorting the Ride history
         kansasTwister.sortRideHistory(new VisitorComparator());
+        System.out.println();
 
         // Printing all Visitors in the Ride history again after Sorting is done
         System.out.println("After sorting:");
         kansasTwister.PrintRideHistory();
+        System.out.println();
+
+        System.out.println("PART 5---------------------------------");
+    }
+
+    public void partFive() {
+        // Part 5
+        // New Ride object
+        Ride hyperCoaster = new Ride("HyperCoaster", 10, new Employee("Emily Johnson", 27, "Female", "E126", "Ride Operator"), 5);
+
+        // Visitor objects
+        Visitor visitor1 = new Visitor("James", 21, "Male", "V016", "Regular");
+        Visitor visitor2 = new Visitor("Anne", 22, "Female", "V017", "Regular");
+        Visitor visitor3 = new Visitor("Sukman", 23, "Male", "V018", "Regular");
+        Visitor visitor4 = new Visitor("Frank", 24, "Male", "V019", "Regular");
+        Visitor visitor5 = new Visitor("Alice", 25, "Female", "V020", "Regular");
+        Visitor visitor6 = new Visitor("Andria", 26, "Female", "V021", "Regular");
+        Visitor visitor7 = new Visitor("Ernest", 27, "Male", "V022", "Regular");
+        Visitor visitor8 = new Visitor("Joyce", 28, "Female", "V023", "Regular");
+        Visitor visitor9 = new Visitor("Richard", 29, "Male", "V024", "Regular");
+        Visitor visitor10 = new Visitor("Lan", 30, "Female", "V025", "Regular");
+
+        // Adding Visitors to the Queue
+        hyperCoaster.AddVisitorToQueue(visitor1);
+        hyperCoaster.AddVisitorToQueue(visitor2);
+        hyperCoaster.AddVisitorToQueue(visitor3);
+        hyperCoaster.AddVisitorToQueue(visitor4);
+        hyperCoaster.AddVisitorToQueue(visitor5);
+        hyperCoaster.AddVisitorToQueue(visitor6);
+        hyperCoaster.AddVisitorToQueue(visitor7);
+        hyperCoaster.AddVisitorToQueue(visitor8);
+        hyperCoaster.AddVisitorToQueue(visitor9);
+        hyperCoaster.AddVisitorToQueue(visitor10);
+        System.out.println();
+
+        // Print all Visitors in the Queue
+        System.out.println("Visitors in the waiting line:");
+        hyperCoaster.PrintQueue();
+        System.out.println();
+
+        // Run one cycle
+        hyperCoaster.RunOneCycle();
+        System.out.println();
+
+        // Print all Visitors in the Queue after one cycle
+        System.out.println("Visitors in the waiting line after one cycle:");
+        hyperCoaster.PrintQueue();
+        System.out.println();
+
+        // Print all Visitors in the Ride history
+        System.out.println("Visitors in the ride history:");
+        hyperCoaster.PrintRideHistory();
+        System.out.println();
+
+        System.out.println("Part 6---------------------------------");
     }
 
 
--- 24422219-A2/src/Ride.java
+++ 24422219-A2/src/Ride.java
@@ -10,6 +10,9 @@ public class Ride implements RideInterface {
     private Employee operator;  // This variable will be used to know if the ride is open and who is operating it
     private Queue<Visitor> waitingLine; // Visitor Waiting List
     private LinkedList<Visitor> rideHistory; // This is used to keep the record on Ride History
+    private int maxRider;
+    private int numOfCycles;
+
 
     // Default constructor
     public Ride() {
@@ -18,12 +21,14 @@ public class Ride implements RideInterface {
     }
 
     // Constructor parameters
-    public Ride(String rideName, int capacity, Employee operator) {
+    public Ride(String rideName, int capacity, Employee operator, int maxRider) {
         this.rideName = rideName;
         this.capacity = capacity;
         this.operator = operator;
         this.waitingLine = new LinkedList<>();
         this.rideHistory = new LinkedList<>();
+        this.maxRider = maxRider;
+        this.numOfCycles = 0;
     }
 
     // Getters and Setters
@@ -54,6 +59,22 @@ public class Ride implements RideInterface {
     // Method to assign an Employee to operate the ride
     public void assignOperator(Employee operator) {
         this.operator = operator;
+    }
+
+    public int getMaxRider() {
+        return maxRider;
+    }
+
+    public void setMaxRider(int maxRider) {
+        this.maxRider = maxRider;
+    }
+
+    public int getNumOfCycles() {
+        return numOfCycles;
+    }
+
+    public void setNumOfCycles(int numOfCycles) {
+        this.numOfCycles = numOfCycles;
     }
 
     // Add Visitor to Queue using Offer method
@@ -121,6 +142,32 @@ public class Ride implements RideInterface {
     public void sortRideHistory(Comparator<Visitor> comparator) {
         Collections.sort(rideHistory, comparator);
         System.out.println("Ride history sorted.");
+    }
+
+    //@Override
+    public void RunOneCycle() {
+        if (operator == null) {
+            System.out.println("No operator assigned. You can not run the ride.");
+            return;
+        }
+
+        if (waitingLine.isEmpty()) {
+            System.out.println("No visitors in the waiting line. Can not run the ride.");
+            return;
+        }
+
+        System.out.println("Running one cycle...");
+        int visitorsToRide = Math.min(maxRider, waitingLine.size());
+        for (int i = 0; i < visitorsToRide; i++) {
+            Visitor visitor = waitingLine.poll();
+            if (visitor != null) {
+                rideHistory.add(visitor);
+                System.out.println(visitor.getName() + " has taken the ride.");
+            }
+        }
+
+        numOfCycles++;
+        System.out.println("Run Cycle completed. Total cycles run: " + numOfCycles);
     }
 
 
